21512450ead1b6ad4eb227b8398f95f766ce1db5
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2023-11-11 23:57:15 +0530

    todo-list with typescript

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var buttonElement = document.querySelector('button'); // it recognizes HTML content , ! mark represents the expression is theoritically null but it isn't.
-function add(num1, num2) {
-    if (typeof num1 === 'number' && num2 === 'number') {
-        return num1 + num2;
-    }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + ' ' + num2;
-    }
-    return +num1 + +num2; // if one is num and other string then we convert into num
-}
-buttonElement.addEventListener('click', function () {
-    var num1 = num1Element.value; // value always return the string
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2); // we forced to covert to integer
-    var stringResult = add(num1, num2);
-    console.log(stringResult);
-    console.log(result);
-});
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
+app.listen(3000);
--- app.ts
+++ app.ts
@@ -1,58 +1,13 @@
-const num1Element = document.getElementById('num1') as HTMLInputElement;
-const num2Element = document.getElementById('num2') as HTMLInputElement;
-const buttonElement = document.querySelector('button') !; // it recognizes HTML content , ! mark represents the expression is theoritically null but it isn't.
+import express from 'express';
 
-const numResults: Array<number> = [];
-const textResults:  string[]= [];
+import bodyParser from 'body-parser';
 
-type NumOrString = number | string ; //type alias
-type Result = { val : number ; timestamp : Date};
+import todosRoutes from './routes/todos';
 
-//interfaces
-interface resultObj {
-    val : number;
-    timestamp : Date;
-}
+const app = express();
 
-function  add(num1: NumOrString, num2: NumOrString) {
-    if(typeof num1 === 'number' && num2 === 'number')
-    {
-        return num1 + num2;
-    }
-    else if(typeof num1 === 'string' && typeof num2 === 'string')
-    {
-        return num1 + ' ' + num2;
-    }
-    return +num1 + +num2; // if one is num and other string then we convert into num
-}
+app.use(bodyParser.json());
 
-function printResult(resultObj : Result) {
+app.use(todosRoutes);
 
-    console.log(resultObj.val);
-}
-
-
- buttonElement.addEventListener('click' , () => {
-
-    const num1 = num1Element.value ; // value always return the string
-    const num2 = num2Element.value ;
-    const result = add(+num1 , +num2); // we forced to covert to integer
-    numResults.push(result as number);
-    const stringResult = add(num1,num2);
-    textResults.push(stringResult as string);
-    console.log(stringResult);
-    console.log(result);
-    printResult({val : result as number , timestamp : new Date()})
-    console.log(numResults , textResults);
-});
-
-
-const myPromise = new Promise<string>((resolve ,reject) => {
-    setTimeout(() => {
-        resolve('IT worked!')
-    },1000);
-})
-
-myPromise.then((result) => {
-    console.log(result.split('w'));
-})
+app.listen(3000);
